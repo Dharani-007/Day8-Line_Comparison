afd4fc8d6084ced529b29064ca23ab7130a06fa4
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-04 00:27:02 +0530

    Added : Compare two lines

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,39 +2,41 @@ public class LineComparison {
     public static double firstLineLength;
     public static double secondLineLength;
 
-    // Method for calculating the length of line.
+    /**
+     * Method for calculating the length of line.
+     */
     private static double calculatingLengthOfLine(int x1, int x2, int y1, int y2) {
         double lineLength;
         lineLength = Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2)));
         return lineLength;
-
     }
 
-    //Method for checking equality of two lines by comparing their lengths
-    private static void compareLine() {
-        if (firstLineLength == secondLineLength) {
-            String stringFirst = String.valueOf(firstLineLength);
-            String stringSecond = String.valueOf(secondLineLength);
-            int check = stringFirst.compareTo(stringSecond);
-            if (check == 0) {
+    /**
+     * Method for checking equality of two lines.
+     */
+    private static void checkingEqualityOfLine() {
+        private static void compareLine () {
+            if (firstLineLength == secondLineLength) {
                 System.out.println("Both Lines are equal");
             } else {
+                System.out.println("Lines are not equal");
                 if (firstLineLength > secondLineLength) {
-                    if (check > 0) {
-                        System.out.println("First line is longer than second line");
-                    } else {
-                        System.out.println("Second line is longer than First line");
-                    }
+                    System.out.println("First line is longer than second line");
+                } else {
+                    System.out.println("Second line is longer than First line");
                 }
             }
         }
     }
-    public static void main(String[] args) {
-        System.out.println("Welcome To Line Comparison Computation Program on Master Branch");
-        firstLineLength = calculatingLengthOfLine(3, 2, 6, 8);
-        System.out.println(firstLineLength);
-        secondLineLength = calculatingLengthOfLine(5, 3, 6, 2);
-        System.out.println(secondLineLength);
-        compareLine();
+
+        public static void main(String[] args) {
+            System.out.println("Welcome To Line Comparison Computation Program on Master Branch");
+            firstLineLength = calculatingLengthOfLine(3, 2, 6, 8);
+            System.out.println(firstLineLength);
+            secondLineLength = calculatingLengthOfLine(5,3,6,2);
+            secondLineLength = calculatingLengthOfLine(5, 3, 6, 2);
+            System.out.println(secondLineLength);
+            checkingEqualityOfLine();
+            compareLine();
+        }
     }
-}
