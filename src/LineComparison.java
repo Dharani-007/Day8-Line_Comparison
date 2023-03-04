7d94185198f9f47a0f9b719aaa13cedb6ee4a198
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-04 07:58:59 +0530

    Added : Equality of two line

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Problem");
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of co-ordinate");
         System.out.println("Enter the value of (x1,y1)= ");
@@ -12,8 +10,29 @@ public class LineComparison {
         System.out.println("Enter the value of (x2,y2)= ");
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
+//length of the line
+        double distanceLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("length of Line1= " + distanceLine1);
 
-        double distanceLine1= Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
-        System.out.println("length of Line1= "+ distanceLine1);
+        System.out.println("Enter the values of next co-ordinates ");
+        System.out.println("x3= ");
+        int x3 = sc.nextInt();
+        System.out.println("y3= ");
+        int y3 = sc.nextInt();
+        System.out.println("x4= ");
+        int x4 = sc.nextInt();
+        System.out.println("y4= ");
+        int y4 = sc.nextInt();
+
+        double distanceLine2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
+        System.out.println("length of Line1= " + distanceLine1);
+        System.out.println("length of Line2= " + distanceLine2);
+//checks if distanceLine1 is equal to distanceLine2 using an if statement
+        if (distanceLine1 == distanceLine2) {
+            System.out.println("Two Lines are Equal");
+        } else {
+            System.out.println("Two Lines are Not Equal");
+        }
     }
 }
